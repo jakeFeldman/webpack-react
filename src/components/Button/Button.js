7d34a4f920f8d17b0e2e35b835b7c7e7fe83d1ba
jakeFeldman <jacob.h.feldman@gmail.com>
2019-02-06 10:21:13 -0700

    Add counter example

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,5 @@
 // PACKAGES
-import React from 'react';
+import React, { useState } from 'react';
 // STYLES
 import './Button.scss';
 
@@ -9,7 +9,12 @@ import './Button.scss';
  * @author Jake Feldman
  */
 export default function Button ({ children }) {
+    const [count, setCount] = useState(0);
     return (
-        <button className="button">{children}</button>
+        <React.Fragment>
+            <p>I've been clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)} className="button">{children}</button>
+            <button onClick={() => setCount(count - 1)} className="button">Decrement</button>
+        </React.Fragment>
     );
 }
